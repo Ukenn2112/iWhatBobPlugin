1d7a398d232477c31e0ccd1d30b6985b8c4b6dda
Ukenn <duola2002@gmail.com>
2023-03-13 23:34:59 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+<img src="./src/icon.png" align="left" width="65"> <h1>iWhat 这是什么 Bob Plugin</h1>
+
+## 功能展示
+
+<img src="" align="center" width="800">
+
+## 使用方法
+
+1、安装 [Bob](https://bobtranslate.com/guide/#%E5%AE%89%E8%A3%85) (版本 >= 0.50)
+
+2、前往 [Releases](https://github.com/Ukenn2112/iWhatBobPlugin/releases) 页下载此插件
+
+3、双击下载的 `iwhat.bobplugin` 安装此插件
+
+## 感谢
+
+- 基于 [yihong0618/iWhat](https://github.com/yihong0618/iWhat) 的扩展 感谢 [@yihong0618](https://github.com/yihong0618)
+- 感谢 Bob 作者 [@ripperhe](https://github.com/ripperhe)
+- 由于从来没有写过 JavaScript 此插件是照着 [@yetone](https://github.com/yetone) 的代码上进行修改，特此感谢
---
+++ src/main.js
@@ -0,0 +1,121 @@
+var lang = require("./lang.js");
+
+function supportLanguages() {
+  return lang.supportLanguages.map(([standardLang]) => standardLang);
+}
+
+function translate(query, completion) {
+  const queryTxt = query.text.replace("，", "，\r\n").replace("。", "。\r\n").replace(",", ",\r\n").replace(".", ".\r\n").trim();
+  let whatPrompt = `这个 "${queryTxt}" 可能是什么，请按照json格式回答，key值有Maybe和Desc，Maybe回答他最可能是的东西（要求精确些），Desc回答这个东西的描述;\n` + 
+                   `答案应该使用中文。`;
+  if ($option.back_language === "en") {
+    whatPrompt = `What is "${queryTxt}" might be? please answer in JSON format with key values of 'Maybe' and 'Desc'. \n` + 
+                  `'Maybe' should provide the most likely thing it is (be more precise), \n` + 
+                  `while 'Desc' should describe what this thing is. \n` + 
+                  `And you answer must be english.`;
+  };
+  (async () => {
+    const targetTxt = await post_openai(whatPrompt);
+    if (targetTxt.error) {
+      completion(targetTxt);
+      return;
+    }
+    try {
+      backData = JSON.parse(targetTxt);
+      $log.info(`返回结果是 JSON: ${JSON.stringify(backData)}`);
+    } catch {
+      $log.info(`返回结果不是 JSON: ${JSON.stringify(targetTxt)}`);
+      if ($option.back_language === "en") {
+        maybePrompt = `What is "${queryTxt}" most likely to be? (Please try to answer more precisely.)`
+        desc_prompt = `Describe what "${queryTxt}" most likely to be`
+      } else {
+        maybePrompt = `这个 "${queryTxt}" 最可能是什么？（要求精确些）`
+        desc_prompt = `描述 "${queryTxt}" 最可能是什么`
+      }
+      const maybeTxt = await post_openai(maybePrompt);
+      const descTxt = await post_openai(desc_prompt);
+      if (maybeTxt.error) {
+        completion(maybeTxt);
+        return;
+      }
+      if (descTxt.error) {
+        completion(descTxt);
+        return;
+      }
+      backData = {
+        Maybe: maybeTxt,
+        Desc: descTxt,
+      };
+    }
+    
+    completion({
+      result: {
+        from: query.detectFrom,
+        to: query.detectTo,
+        toParagraphs: [backData.Desc],
+        toDict: {
+          additions: [
+            { name: "What", value: query.text.trim() },
+            { name: "Maybe", value: backData.Maybe }
+          ],
+        },
+      },
+    });
+
+  })().catch((err) => {
+    completion({
+      error: {
+        type: err._type || "unknown",
+        message: err._message || "未知错误",
+        addition: err._addition,
+      },
+    });
+  });
+}
+
+async function post_openai(prompt) {
+  const header = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${$option.api_key}`,
+  };
+  const body = {
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: prompt }],
+  };
+  const resp = await $http.request({
+    method: "POST",
+    url: $option.api_url + "/v1/chat/completions",
+    header,
+    body,
+  });
+  if (resp.error) {
+    const { statusCode } = resp.response;
+    let reason;
+    if (statusCode >= 400 && statusCode < 500) {
+      reason = "param";
+    } else {
+      reason = "api";
+    }
+    return {
+      error: {
+        type: reason,
+        message: `接口响应错误 - ${resp.data.error.message}`,
+        addition: JSON.stringify(resp),
+      },
+    };
+  } else {
+    const { choices } = resp.data;
+    if (!choices || choices.length === 0) {
+      return {
+        error: {
+          type: "api",
+          message: "接口未返回结果",
+        },
+      };
+    }
+    return choices[0].message.content.trim();
+  }
+}
+
+exports.supportLanguages = supportLanguages;
+exports.translate = translate;
